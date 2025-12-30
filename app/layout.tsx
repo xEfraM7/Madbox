c661273cd5bedb9abc90b7517183c1a6b38291be
xEfraM7 <efraincabrera35@gmail.com>
2025-12-30 08:04:44 -0400

    feat: cambio icono en pestana

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon-light-32x32.svg",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon-dark-32x32.svg",
         media: "(prefers-color-scheme: dark)",
       },
       {
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/apple-icon.svg",
   },
 }
 
